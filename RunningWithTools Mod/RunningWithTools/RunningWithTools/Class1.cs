3a60a3ff1d5569c06f9a69dc39840b544ad54364
Musashi <aceredi@gmail.com>
2019-01-08 04:39:07 -0800

    Add files via upload

---
+++ RunningWithTools Mod/RunningWithTools/RunningWithTools/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.VR;
+using UWE;
+
+namespace RunningWithTools  
+{
+    [HarmonyPatch(typeof(Player))]
+    [HarmonyPatch("UpdateItemHolstering")]
+    internal class Player_UpdateItemHolstering_Patch
+    {
+
+        [HarmonyPrefix]
+        public static bool Player_UpdateItemHolstering_Prefix()
+        {
+
+           
+                return false;
+        }
+
+    }
+}
---
+++ RunningWithTools Mod/RunningWithTools/RunningWithTools/MainPatcher.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Harmony;
+
+// You can use this file almost as-is. Just change the marked lines below. This will be the main file of each mod that tells Harmony to load your changes.
+namespace RunningWithTools     // Change this line to match your mod.
+{
+    public class MainPatcher
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.oldark.subnautica.runningwithtools.mod");   // Change this line to match your mod. 
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
